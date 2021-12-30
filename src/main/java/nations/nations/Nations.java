7c72899a6b1b61c9b31bc892aa0627fd3b097a19
Elboron <eldariontheseafarer@gmail.com>
2021-12-30 22:14:53 +0100

    Initial commit

---
+++ src/main/java/nations/nations/Nations.java
@@ -0,0 +1,20 @@
+package nations.nations;
+
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("nations")
+public class Nations {
+
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public Nations() {
+
+    }
+
+}
